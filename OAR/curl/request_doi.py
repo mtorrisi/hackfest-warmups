5b474a7e2cef4e4fae24942b8847770c02330265
mtorrisi <torrisi.mario@gmail.com>
2017-02-14 13:52:33 +0100

    Little refactor to the request_doi.py

--- OAR/curl/request_doi.py
+++ OAR/curl/request_doi.py
@@ -2,13 +2,8 @@
 
 # A simple python script that create a DOI based on system timestamp.
 
-import json,cgi,time
-import httplib2, sys, base64, codecs
+import cgi,time
 
-res=[]
-retCode=0
-errCode=''
-doi='11623'
-res =  "%s/sci-gaia:%s" % (doi,time.time())
-print "Content-type: application/json\n\n"
-print json.dumps(res)
+doi_prefix='11623'
+doi =  "%s/sci-gaia:%s" % (doi_prefix,time.time())
+print doi
